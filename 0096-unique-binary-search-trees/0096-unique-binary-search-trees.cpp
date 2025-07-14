80d03bb7b838b0f28c87309e49b8e886b199dc4d
Mayank <151539657+01mayankk@users.noreply.github.com>
2025-07-15 01:02:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+#include <cmath>  // for tgamma
+class Solution {
+public:
+    int numTrees(int n) {
+        // Catalan number: C(n) = (2n)! / (n+1)! * n!
+        // Use tgamma(n+1) == n!
+
+        long long numerator = tgamma(2*n + 1);     
+        long long denominator = tgamma(n + 2) * tgamma(n + 1);  
+
+        return static_cast<int>(numerator / denominator + 0.5);  // round to nearest int
+    }
+};
