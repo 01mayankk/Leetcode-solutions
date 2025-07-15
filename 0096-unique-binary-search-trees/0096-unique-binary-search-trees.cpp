2ead75c23eaf1c98518a300317600c79e7c03062
Mayank <151539657+01mayankk@users.noreply.github.com>
2025-07-15 10:31:20 +0530

    Time: 2024 ms (10.99%), Space: 7.8 MB (78.52%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,34 @@
-#include <cmath>  // for tgamma
+// #include <cmath>      
+// class Solution {
+// public:
+//     int numTrees(int n) {
+//         // Catalan number: C(n) = (2n)! / (n+1)! * n!
+//         // Use tgamma(n+1) == n!
+
+//         double numerator = tgamma(2*n + 1);     
+//         double denominator = tgamma(n + 2) * tgamma(n + 1);  
+
+//         return static_cast<int>(numerator / denominator + 0.5);  // round to nearest int
+//     }
+// };
+
+
+
+// //we can also go for boost::multiprecision 
+// // there we can define a variable using cpp_int variablename;
+// //then calculate the value as it will safely return the factorial till n > 20;
 class Solution {
 public:
     int numTrees(int n) {
-        // Catalan number: C(n) = (2n)! / (n+1)! * n!
-        // Use tgamma(n+1) == n!
+        if (n <= 1)
+            return 1;
 
-        double numerator = tgamma(2*n + 1);     
-        double denominator = tgamma(n + 2) * tgamma(n + 1);  
-
-        return static_cast<int>(numerator / denominator + 0.5);  // round to nearest int
+        int total = 0;
+        for (int i = 1; i <= n; ++i) {
+            int left = numTrees(i - 1);
+            int right = numTrees(n - i);
+            total += left * right;
+        }
+        return total;
     }
 };
