40c49a5b44b518ac291e19a121cac68707cc3f2b
Mayank <151539657+01mayankk@users.noreply.github.com>
2025-07-17 09:48:30 +0530

    Time: 7 ms (16.54%), Space: 18 MB (53.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,3 +1,34 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+//         unordered_map<int, int>map;
+//         vector<int>result;
+                
+//         for(int i = 0; i < nums.size() ; i++)
+//         {
+//             map[nums[i]]++;
+//         }
+
+//         priority_queue<pair<int, int>>max;
+
+//         for(auto it = map.begin(); it != map.end(); it++)
+//         {
+//             max.push(make_pair(it->second, it->first));
+//         }
+
+//         for(int i = 0; i < k; i++)
+//         {
+//             result.push_back(max.top().second);
+//             max.pop();
+//         }
+
+//         return result;
+//     }
+// };
+
+//Above we are using max heap to get the max frequent element on top but we can also go for min heap in the time complexity will reduce to O(n log K) will in max it will always be O(n log n).
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
