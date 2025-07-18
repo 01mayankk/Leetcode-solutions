62abf112b21b03da26aacce0a573bbdabdf23eb9
Mayank <151539657+01mayankk@users.noreply.github.com>
2025-07-18 09:07:19 +0530

    Time: 1 ms (33.36%), Space: 8.4 MB (22.8%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        unordered_set<int> seen;
+
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+        
+        return n == 1;
+    }
+
+private:
+    int getNext(int number) {
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10; 
+            sum += digit * digit; 
+            number /= 10; 
+        }
+        return sum; 
+    }
+};
