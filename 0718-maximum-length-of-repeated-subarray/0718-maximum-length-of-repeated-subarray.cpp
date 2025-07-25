51255e90522c2deee51b311d84bae14c41fefc3e
Mayank <151539657+01mayankk@users.noreply.github.com>
2025-07-25 15:12:43 +0530

    Time: 103 ms (32.75%), Space: 89.2 MB (68.29%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,24 +1,61 @@
+// class Solution {
+// public:
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//         int n = nums1.size(), m = nums2.size();
+//         int maxLen = 0;
+
+//         for(int i = 0; i < n; ++i) {
+//             for(int j = 0; j < m; ++j) {
+//                 int len = 0;
+//                 int x = i, y = j;
+
+//                 while(x < n && y < m && nums1[x] == nums2[y]) {
+//                     len++;
+//                     x++;
+//                     y++;
+//                 }
+
+//                 maxLen = max(maxLen, len);
+//             }
+//         }
+
+//         return maxLen;
+//     }
+// };
+
+//Above is a brute force where we are matching each element of nums1 to nums2 one by one which give TLE as it will take O((m *n)* min(m, n))
+
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
+        
+        if(n == 0 || m == 0)
+        {
+            return 0;
+        }
         int maxLen = 0;
 
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                int len = 0;
-                int x = i, y = j;
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
 
-                while(x < n && y < m && nums1[x] == nums2[y]) {
-                    len++;
-                    x++;
-                    y++;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j <= m ;j++)
+            {
+                if(nums1[i-1] == nums2[j-1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j-1];
                 }
 
-                maxLen = max(maxLen, len);
+            maxLen = max(maxLen, dp[i][j]);
+
             }
+
         }
 
         return maxLen;
+        
+
+        
     }
 };
