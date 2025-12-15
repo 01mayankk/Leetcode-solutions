865448dd49267099522aa86fd6492b3cb85e6268
Mayank <151539657+01mayankk@users.noreply.github.com>
2025-12-16 01:24:05 +0530

    Time: 3 ms (67.78%), Space: 14.7 MB (53.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        unordered_map<int, int>map;
+        unordered_map<int, int>map;// make map to store value 
 
         for(int i = 0; i < nums.size(); i++)
         {
-            int complement = target - nums[i];
-            if (map.find(complement) != map.end()) {
+            int complement = target - nums[i]; // make a complement 
+            if (map.find(complement) != map.end()) { // check whether complement already seen or not if not mp.end() will return the iterator means the last elemnt is reached of the map
                 return {map[complement], i};
             }
-            map[nums[i]] = i;
+            map[nums[i]] = i; //other wise push the element as key and index as value;
 
             
         }
