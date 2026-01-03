4042d629ddd779f68f018272fd5e2010cbdadeb6
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-01-03 09:51:42 +0530

    Time: 7 ms (73.16%), Space: 22.9 MB (76.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,42 +2,31 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
 
-        // Minimum possible eating speed (bananas/hour)
-        int low = 1;
+        int minSpeed = 1;
 
-        // Maximum possible eating speed
-        // If Koko eats the largest pile in 1 hour
-        int high = *max_element(piles.begin(), piles.end());
+        int maxSpeed = *max_element(piles.begin(), piles.end());
 
-        // Binary search on the answer space [low, high]
-        while (low < high) {
+        while(minSpeed < maxSpeed)
+        {
+            int midSpeed = minSpeed + (maxSpeed - minSpeed) / 2;
 
-            // Try mid as current eating speed
-            int mid = (low + high) / 2;
-
-            // Total hours needed at speed = mid
             int hours = 0;
 
-            // Calculate hours required for each pile
-            for (int pile : piles) {
-                // Ceiling division:
-                // (pile + mid - 1) / mid
-                // ensures partial bananas count as full hour
-                hours += (pile + mid - 1) / mid;
+            for(int pile : piles)
+            {
+                hours += (pile + midSpeed - 1) / midSpeed;
             }
 
-            // If Koko can finish within h hours
-            if (hours <= h) {
-                // Try smaller speed
-                high = mid;
-            } 
-            else {
-                // Need higher eating speed
-                low = mid + 1;
+            if(hours <= h)
+            {
+                maxSpeed = midSpeed;
             }
-        }
+            else
+            {
+                minSpeed = midSpeed + 1;
+            }
+        }  
 
-        // low will be the minimum valid eating speed
-        return low;
+        return minSpeed; 
     }
 };
