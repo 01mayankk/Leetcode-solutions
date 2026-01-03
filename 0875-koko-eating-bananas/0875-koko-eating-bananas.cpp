d04d28fa6ff978bdf6d398331970a8fc01d4f2bc
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-01-03 09:51:22 +0530

    Time: 8 ms (57.66%), Space: 23 MB (43.76%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,24 +2,42 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
 
-     int low = 1;
+        // Minimum possible eating speed (bananas/hour)
+        int low = 1;
+
+        // Maximum possible eating speed
+        // If Koko eats the largest pile in 1 hour
         int high = *max_element(piles.begin(), piles.end());
 
+        // Binary search on the answer space [low, high]
         while (low < high) {
+
+            // Try mid as current eating speed
             int mid = (low + high) / 2;
+
+            // Total hours needed at speed = mid
             int hours = 0;
 
+            // Calculate hours required for each pile
             for (int pile : piles) {
-                hours += (pile + mid - 1) / mid;  
+                // Ceiling division:
+                // (pile + mid - 1) / mid
+                // ensures partial bananas count as full hour
+                hours += (pile + mid - 1) / mid;
             }
 
+            // If Koko can finish within h hours
             if (hours <= h) {
+                // Try smaller speed
                 high = mid;
-            } else {
+            } 
+            else {
+                // Need higher eating speed
                 low = mid + 1;
             }
         }
 
+        // low will be the minimum valid eating speed
         return low;
     }
 };
