043967fe8b297c925fb806f89cb41c183b19e17c
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-02-10 13:37:42 +0530

    Time: 52 ms (81.84%), Space: 23.8 MB (98.61%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,24 +6,36 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        // If either list is empty, no intersection possible
-        if (!headA || !headB) return nullptr;
 
-        // Two pointers starting at heads of both lists
-        ListNode* pA = headA;
-        ListNode* pB = headB;
+        // If either list is empty, they can never intersect
+        if (headA == nullptr || headB == nullptr)
+            return nullptr;
 
-        // Traverse until they meet or both become null
-        while (pA != pB) {
-            // Move pointer forward; if it reaches end, switch to other list's head
-            pA = (pA == nullptr) ? headB : pA->next;
-            pB = (pB == nullptr) ? headA : pB->next;
+        // Two pointers starting at the heads of both lists
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        // Move both pointers until they meet
+        while (temp1 != temp2) {
+
+            // If temp1 reaches the end of list A,
+            // redirect it to the head of list B
+            // otherwise move to next node
+            temp1 = (temp1 == nullptr) ? headB : temp1->next;
+
+            // If temp2 reaches the end of list B,
+            // redirect it to the head of list A
+            // otherwise move to next node
+            temp2 = (temp2 == nullptr) ? headA : temp2->next;
         }
 
-        // Either intersection node or nullptr if no intersection
-        return pA;
+        // When loop exits:
+        // - Either both point to the intersection node
+        // - Or both are nullptr (no intersection)
+        return temp1;
     }
 };
