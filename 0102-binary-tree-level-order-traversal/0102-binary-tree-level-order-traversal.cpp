06b5b0f943b808aa092c048798361a228c29cfe4
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-02-21 12:55:42 +0530

    Time: 1 ms (40.55%), Space: 17 MB (71.36%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,32 +12,61 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans; 
-        if (!root) return ans;
+        
+        // This will store the final level order traversal result
+        vector<vector<int>> result;
 
+        // Base case: if tree is empty, return empty result
+        if(root == nullptr)
+        {
+            return {};
+        }
+
+        // Queue is used for BFS (Level Order Traversal)
         queue<TreeNode*> q;
+
+        // Start by pushing the root node into queue
         q.push(root);
 
-        while (!q.empty()) {
-            int size = q.size(); 
-            vector<int> level;   
+        // Continue traversal until queue becomes empty
+        while(!q.empty())
+        {
+            // Number of nodes at current level
+            int len = q.size();
 
-            while (size--) {
+            // This vector will store values of current level
+            vector<int> ans;
+
+            // Process all nodes of current level
+            while(len--)
+            {
+                // Get the front node of queue
                 TreeNode* temp = q.front();
-                q.pop();
-                level.push_back(temp->val);
 
-                if (temp->left) {
+                // Remove it from queue (very important in BFS)
+                q.pop();
+
+                // Store its value in current level vector
+                ans.push_back(temp->val);
+
+                // If left child exists, push it into queue
+                if(temp->left)
+                {
                     q.push(temp->left);
                 }
-                if (temp->right) {
+
+                // If right child exists, push it into queue
+                if(temp->right)
+                {
                     q.push(temp->right);
                 }
             }
 
-            ans.push_back(level); 
+            // After finishing one level, add it to result
+            result.push_back(ans);
         }
 
-        return ans;
+        // Return final 2D vector containing level order traversal
+        return result;
     }
 };
