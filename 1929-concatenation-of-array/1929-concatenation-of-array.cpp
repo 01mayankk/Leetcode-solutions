24b58af6280e1416baed8830176af15c085dca9a
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-02-23 15:34:37 +0530

    Time: 0 ms (100%), Space: 17.2 MB (16.08%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int>ans;
-        int n = nums.size();
-        for(int i = 0; i < n; i++)
-        {
-            ans.push_back(nums[i]);
-            if(i == n - 1)
-            {
-                for(int i = 0; i < n; i++)
-                {
-                    ans.push_back(nums[i]);
+        
+        // This vector will store the final concatenated result
+        vector<int> ans;
 
-                }
-            }
+        // Store the size of the original array
+        int len = nums.size();
+
+        // We need to iterate 2 * len times
+        // because we want to append the array to itself (nums + nums)
+        for(int i = 0; i < 2 * len; i++)
+        {
+            // i % len ensures that:
+            // - For first len elements, it copies normally
+            // - For next len elements, it starts again from index 0
+            // This effectively duplicates the array
+            ans.push_back(nums[i % len]);
         }
 
+        // Return the concatenated vector
         return ans;
     }
 };
