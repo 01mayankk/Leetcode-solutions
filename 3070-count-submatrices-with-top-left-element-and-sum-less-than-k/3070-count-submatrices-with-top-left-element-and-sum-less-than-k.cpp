c2a8de82a0543ae62dfed5bf6eb24df9938feb76
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-03-18 18:37:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
+        // Get dimensions of the grid
         int n = grid.size();
         int m = grid[0].size();
         
-        // Create prefix sum matrix
+        // Prefix sum matrix to store sum from (0,0) to (i,j)
         vector<vector<long long>> prefix(n, vector<long long>(m, 0));
         
-        int count = 0;
+        int count = 0; // To store number of valid submatrices
         
+        // Traverse each cell
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 
-                // Current cell value
+                // Start with current cell value
                 prefix[i][j] = grid[i][j];
                 
-                // Add top
+                // Add sum from the cell above (if exists)
                 if (i > 0)
                     prefix[i][j] += prefix[i - 1][j];
                 
-                // Add left
+                // Add sum from the left cell (if exists)
                 if (j > 0)
                     prefix[i][j] += prefix[i][j - 1];
                 
-                // Remove double counted area
+                // Subtract the overlapping top-left part 
+                // (because it was added twice)
                 if (i > 0 && j > 0)
                     prefix[i][j] -= prefix[i - 1][j - 1];
                 
-                // Check if sum from (0,0) to (i,j) is <= k
+                /*
+                    Now prefix[i][j] represents the sum of submatrix
+                    from (0,0) to (i,j)
+                    
+                    If this sum is <= k, it is a valid submatrix
+                */
                 if (prefix[i][j] <= k)
                     count++;
             }
         }
         
+        // Return total count of valid submatrices
         return count;
     }
 };
