278e5fcb1a25164297c9cd8f4a8e90a4c09a6230
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-04-29 22:33:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,8 +2,33 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        // 'candidate' stores the current majority candidate
+        // 'vote' keeps track of its frequency balance
+        int candidate = nums[0];
+        int vote = 1;
+        
+        // Traverse the array starting from index 1
+        for(int i = 1; i < nums.size(); i++)
+        {
+            // If vote becomes 0, we choose a new candidate
+            if(vote == 0)
+            {
+                candidate = nums[i];
+                vote = 1;
+            }
+            // If current element matches candidate → increase vote
+            else if(nums[i] == candidate)
+            {
+                vote++;
+            }
+            // If different → decrease vote (cancel out effect)
+            else
+            {
+                vote--;
+            }
+        }
 
+        // The remaining candidate is the majority element
+        return candidate;
     }
 };
