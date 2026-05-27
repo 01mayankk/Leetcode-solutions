d0f6a8d197d6b2373a38edab664021cedadc716a
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-05-27 21:50:41 +0530

    Time: 3 ms (68.29%), Space: 14.7 MB (9.19%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        
-        vector<int>result;
-        int count = 0;
 
+        // Create a copy of original array
+        // because nums will be sorted later
+        vector<int> copy(nums.begin(), nums.end());
+
+        // Sort the array
+        // Sorted order helps determine how many
+        // numbers are smaller than a particular number
+        sort(nums.begin(), nums.end());
+
+        // HashMap:
+        // key   -> number
+        // value -> count of smaller numbers
+        unordered_map<int, int> mp;
+
+        // Traverse sorted array
         for(int i = 0; i < nums.size(); i++)
         {
-            for(int j = 0; j < nums.size(); j++)
+            // Store only first occurrence
+            // because first index in sorted array
+            // equals count of smaller elements
+            //
+            // Example:
+            // Sorted: [1,2,2,3,8]
+            // First occurrence of 2 is at index 1
+            // => only 1 number is smaller than 2
+            if(mp.find(nums[i]) == mp.end())
             {
-                if(i == j)
-                {
-                    continue;
-                }
-                if(nums[i] > nums[j])
-                {
-                    count++;
-                }
+                mp[nums[i]] = i;
             }
-            result.push_back(count);
-            count = 0;
         }
 
-        return result;
+        // Final answer array
+        vector<int> ans;
+
+        // Traverse original array
+        for(int num : copy)
+        {
+            // Push count of smaller numbers
+            // using hashmap
+            ans.push_back(mp[num]);
+        }
+
+        // Return final answer
+        return ans;
     }
 };
