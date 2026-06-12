5624ccdde5a5ff38e3c4543a7ae802000b0fbcec
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-06-13 04:32:17 +0530

    Time: 23 ms (16.8%), Space: 14.4 MB (20.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+
+        // Stores characters currently present in the window
         unordered_set<char> unique;
-        int left = 0, right = 0;
+
+        // Two pointers for Sliding Window
+        int left = 0;
+        int right = 0;
+
+        // Stores length of longest substring without repeating characters
         int maxLen = 0;
 
+        // Expand the window using right pointer
         while (right < s.length()) {
+
+            // If current character is not present in the window
             if (unique.find(s[right]) == unique.end()) {
+
+                // Add current character to the set
                 unique.insert(s[right]);
+
+                // Update maximum length found so far
                 maxLen = max(maxLen, right - left + 1);
+
+                // Expand window
                 right++;
-            } else {
+            }
+            else {
+
+                // Duplicate character found
+                // Remove characters from left side
+                // until duplicate is removed
                 unique.erase(s[left]);
                 left++;
             }
