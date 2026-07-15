cf898d8537ff9d4746e58d45a50d273f3a876810
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-07-15 10:06:01 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        
+        // int evenSum = 0, oddSum = 0;
+
+        // evenSum = n *  ( n + 1);
+        // oddSum = n * n;
+
+        // return gcd(evenSum, oddSum);
+
+        return n;
+    }
+};
