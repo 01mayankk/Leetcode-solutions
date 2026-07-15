f6db6a889360784e3fe1266943b5935827511b8b
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-07-15 10:16:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,14 +1,28 @@
+// class Solution {
+// public:
+//     int gcdOfOddEvenSums(int n) {
+
+//         // Sum of the first n odd numbers = n²
+//         int oddSum = n * n;
+
+//         // Sum of the first n even numbers = n(n + 1)
+//         int evenSum = n * (n + 1);
+
+//         // Return the GCD of both sums
+//         return gcd(oddSum, evenSum);
+//     }
+// };
+
+
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
 
-        // Sum of the first n odd numbers = n²
-        int oddSum = n * n;
+        // gcd(n², n(n+1))
+        // = n * gcd(n, n+1)
+        // Consecutive numbers are always coprime.
+        // Therefore, gcd = n.
 
-        // Sum of the first n even numbers = n(n + 1)
-        int evenSum = n * (n + 1);
-
-        // Return the GCD of both sums
-        return gcd(oddSum, evenSum);
+        return n;
     }
 };
