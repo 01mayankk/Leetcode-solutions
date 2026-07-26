49a2b4b9384147dd2af220df1e2680ea689b0bfb
Mayank <151539657+01mayankk@users.noreply.github.com>
2026-07-26 20:40:51 +0530

    Time: 24 ms (6.63%), Space: 31.5 MB (46.18%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        // Sort the array in non-decreasing order
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        
+        // Option 1: Product of the three largest numbers
+        int option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        
+        // Option 2: Product of the two smallest numbers (which could be negative) and the largest number
+        int option2 = nums[0] * nums[1] * nums[n - 1];
+        
+        // Return the maximum of both options
+        return max(option1, option2);
+    }
+};
